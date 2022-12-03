326c57e6c7ab51c61a636082bb64950a2fef6991
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2022-12-03 11:56:31 +0000

    init commit lol

---
+++ python/day_1.py
@@ -0,0 +1,18 @@
+## Part 1
+with open('./input_1.txt', 'r') as f:
+    elves = {}
+    calories = 0
+    elf = 1
+    for line in f:
+        if line != '\n':
+            calories += int(line)
+        else:
+            elves[elf] = calories
+            calories = 0
+            elf += 1
+
+elves_ranked = dict(sorted(elves.items(), key=lambda item: item[1], reverse=True))
+print(list(elves_ranked.values())[0])
+
+## Part 2
+print(sum(list(elves_ranked.values())[0:3]))
